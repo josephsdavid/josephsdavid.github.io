98eda98c422ef4bef578d82c90a534f863249aea
josephsdavid <josephsd@smu.edu>
2019-12-06 17:34:12 -0600

    did a thing!

--- knit-all.R
+++ knit-all.R
@@ -1,5 +1,6 @@
-library(magrittr)
-library(purrr)
-files <- list.files()
-files %<>% .[grepl(.,pattern = ".Rmd")]
-walk(files, rmarkdown::render)
+#library(magrittr)
+#library(purrr)
+#files <- list.files()
+#files %<>% .[grepl(.,pattern = ".Rmd")]
+#walk(files, rmarkdown::render)
+rmarkdown::render_site()
